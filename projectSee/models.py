2c8b9e649ce6c866bc10c7c7fca5e4c11f33ab63
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-06-12 22:00:14 +0300

    Doc: model classes installed along with pillow

--- projectSee/models.py
+++ projectSee/models.py
@@ -1,6 +1,35 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
 class NewsLetterRecipients(models.Model):
     name = models.CharField(max_length=30)
     email = models.EmailField()
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=50)
+    picture = models.ImageField(null=True, upload_to = 'profiles')
+    Bio = models.TextField(max_length=500, null=False)
+    contacts = models.TextField(max_length=100, null=False)
+
+
+    def __str__(self):
+        return self.name
+
+
+    def save_profile(self):
+        self.save()
+
+
+class Projects(models.Model):
+    title = models.CharField(max_length=50, null=False)
+    image = models.ImageField(null=True, upload_to='projects')
+    description = models.TextField(max_length=300)
+    link = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.title
+
+    def save_projects(self):
+        self.save()
